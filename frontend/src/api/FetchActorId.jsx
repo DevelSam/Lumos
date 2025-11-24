b72074379319a2fccef04414c8148f3f1724f72d
DevSam <maksim2008207@gmail.com>
2025-11-24 11:17:59 +0400

    Update FetchActorId.jsx

--- frontend/src/api/FetchActorId.jsx
+++ frontend/src/api/FetchActorId.jsx
@@ -1,7 +1,7 @@
 import { options } from './apiClient'
 export const FetchActorId = async (id) => {
   try {
-    const response = await fetch(`https://api.kinopoisk.dev/v1.4/person/${id}`, options)
+    const response = await fetch(`https://api.poiskkino.dev/v1.4/person/${id}`, options)
 
     if (!response.ok) {
       throw new Error('Запрос не сработал!')
