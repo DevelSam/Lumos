7998f521269e27468dd6c1582d95bcd2b19ace8e
DevSam <maksim2008207@gmail.com>
2025-11-24 11:18:14 +0400

    Update FetchAllGenre.jsx

--- frontend/src/api/FetchAllGenre.jsx
+++ frontend/src/api/FetchAllGenre.jsx
@@ -2,7 +2,7 @@ import { options } from './apiClient'
 export const FetchAllGenre = async () => {
   try {
     const response = await fetch(
-      'https://api.kinopoisk.dev/v1/movie/possible-values-by-field?field=genres.name',
+      'https://api.poiskkino.dev/v1/movie/possible-values-by-field?field=genres.name',
       options,
     )
     if (!response.ok) {
