3ff6e541468c860a187b2c53ed6fe8e93dc45565
DevSam <maksim2008207@gmail.com>
2025-11-24 11:18:34 +0400

    Update FetchCollection.jsx

--- frontend/src/api/FetchCollection.jsx
+++ frontend/src/api/FetchCollection.jsx
@@ -1,7 +1,7 @@
 import { options } from './apiClient'
 export const FetchCollection = async (list) => {
   try {
-    const response = await fetch(`https://api.kinopoisk.dev/v1.4/list/${list}`, options)
+    const response = await fetch(`https://api.poiskkino.dev/v1.4/list/${list}`, options)
 
     if (!response.ok) {
       throw new Error('Запрос не сработал!')
