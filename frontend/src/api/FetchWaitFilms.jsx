80ccd53e2fe3e8ee3532b0dd54d24d58bf032a5f
DevSam <maksim2008207@gmail.com>
2025-11-24 11:24:59 +0400

    Update FetchWaitFilms.jsx

--- frontend/src/api/FetchWaitFilms.jsx
+++ frontend/src/api/FetchWaitFilms.jsx
@@ -2,7 +2,7 @@ import { options } from './apiClient'
 export const FetchWaitFilms = async () => {
   try {
     const response = await fetch(
-      'https://api.kinopoisk.dev/v1.4/movie?page=1&limit=10&selectFields=&sortField=votes.await&sortType=-1&lists=planned-to-watch-films&notNullFields=poster.url',
+      'https://api.poiskkino.dev/v1.4/movie?page=1&limit=10&selectFields=&sortField=votes.await&sortType=-1&lists=planned-to-watch-films&notNullFields=poster.url',
       options,
     )
 
