a3dd29094b7618c724856f8f53cfea3436249923
DevSam <maksim2008207@gmail.com>
2025-11-24 11:19:39 +0400

    Update FetchPopular.jsx

--- frontend/src/api/FetchPopular.jsx
+++ frontend/src/api/FetchPopular.jsx
@@ -3,7 +3,7 @@ import { options } from './apiClient'
 export const FetchPopular = async () => {
   try {
     const response = await fetch(
-      `https://api.kinopoisk.dev/v1.4/movie?page=1&limit=12&sortField=votes.kp&sortType=-1&votes.kp=1000-6666666&lists=popular-films&year=2022-2024`,
+      `https://api.poiskkino.dev/v1.4/movie?page=1&limit=12&sortField=votes.kp&sortType=-1&votes.kp=1000-6666666&lists=popular-films&year=2022-2024`,
       options,
     )
 
