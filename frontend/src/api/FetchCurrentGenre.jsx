e8c38d31445bba8b360f92195d660258e0190f13
DevSam <maksim2008207@gmail.com>
2025-11-24 11:19:03 +0400

    Update FetchCurrentGenre.jsx

--- frontend/src/api/FetchCurrentGenre.jsx
+++ frontend/src/api/FetchCurrentGenre.jsx
@@ -2,7 +2,7 @@ import { options } from './apiClient'
 export const FetchCurrentGenre = async (genre, type, page) => {
   try {
     const response = await fetch(
-      `https://api.kinopoisk.dev/v1.4/movie?notNullFields=name&notNullFields=poster.url&notNullFields=${type == 'tv-series' ? `seriesLength` : `movieLength`}&isSeries=${type == 'tv-series' ? `true` : `false`}&limit=30&rating.kp=6-10&page=${page}${genre ? `&genres.name=${genre}` : ``}`,
+      `https://api.poiskkino.dev/v1.4/movie?notNullFields=name&notNullFields=poster.url&notNullFields=${type == 'tv-series' ? `seriesLength` : `movieLength`}&isSeries=${type == 'tv-series' ? `true` : `false`}&limit=30&rating.kp=6-10&page=${page}${genre ? `&genres.name=${genre}` : ``}`,
       options,
     )
     if (!response.ok) {
