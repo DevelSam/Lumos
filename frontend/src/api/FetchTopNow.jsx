302e41af26b07174fab208a93b6123368c30137c
DevSam <maksim2008207@gmail.com>
2025-11-24 11:24:42 +0400

    Update FetchTopNow.jsx

--- frontend/src/api/FetchTopNow.jsx
+++ frontend/src/api/FetchTopNow.jsx
@@ -2,7 +2,7 @@ import { options } from './apiClient'
 export const FetchTopNow = async () => {
   try {
     const response = await fetch(
-      'https://api.kinopoisk.dev/v1.4/movie?page=1&limit=5&notNullFields=votes.kp&notNullFields=votes.kp&sortField=votes.kp&sortType=-1&year=2020-2025&lists=popular-films',
+      'https://api.poiskkino.dev/v1.4/movie?page=1&limit=5&notNullFields=votes.kp&notNullFields=votes.kp&sortField=votes.kp&sortType=-1&year=2020-2025&lists=popular-films',
       options,
     )
 
