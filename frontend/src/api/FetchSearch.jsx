3791370c23d86252db853fe9588fe93de4f21a9e
DevSam <maksim2008207@gmail.com>
2025-11-24 11:22:20 +0400

    Update FetchSearch.jsx

--- frontend/src/api/FetchSearch.jsx
+++ frontend/src/api/FetchSearch.jsx
@@ -1,7 +1,7 @@
 import { options } from './apiClient'
 export const FetchSearch = async (query) => {
   try {
-    const res = await fetch(`https://api.kinopoisk.dev/v1.4/movie/search?page=1&limit=5&query=${query}`, options)
+    const res = await fetch(`https://api.poiskkino.dev/v1.4/movie/search?page=1&limit=5&query=${query}`, options)
     if (!res.ok) {
       throw new Error('Ошибка сетевого запроса')
     }
